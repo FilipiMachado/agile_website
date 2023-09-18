8ea4b4b87d6bb13c88598b4a0cf57c5e0deae526
filipi <filipisee@hotmail.com>
2023-09-18 20:01:52 -0300

    finished project setup

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 
+import Navbar from "@/components/navbar";
+import Footer from "@/components/Footer";
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      Navbar
+      <Navbar />
       {children}
-      Footer
+      <Footer />
     </>
   );
 };
