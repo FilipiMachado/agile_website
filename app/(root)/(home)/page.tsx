f3f4acc2a73e6c06bb446e5e40831761164f47a1
filipi <filipisee@hotmail.com>
2023-09-18 20:45:44 -0300

    finished project setup

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div>Home Page</div>
+    <div className="text-white">Home Page</div>
   )
 }
 
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Navbar from "@/components/navbar";
+import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
