c35e75e2240df5e855184a433190c62adffccf49
filipi <filipisee@hotmail.com>
2023-09-19 19:44:29 -0300

    finished filters structure

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,7 +1,31 @@
-const Filters = () => {
-  return (
-    <div>Filters</div>
-  )
-}
+"use client";
 
-export default Filters
+import { useState } from "react";
+
+const links = ["all", "Link 1", "Link 2", "Link 3", "Link 4"];
+
+const Filters = () => {
+  const [active, setActive] = useState("");
+
+  const handleFilter = (link: string) => {
+    setActive(link);
+  };
+
+  return (
+    <ul className="text-white-800 body-text no-scrollbar flex w-full max-w-full gap-2 overflow-auto py-12 sm:max-w-2xl">
+      {links.map((link) => (
+        <button
+          key={link}
+          onClick={() => handleFilter(link)}
+          className={`${
+            active === link ? "gradient_blue-purple hover:text-white-800" : ""
+          } whitespace-nowrap rounded-lg px-8 py-2.5 capitalize hover:text-white-400 transition`}
+        >
+          {link}
+        </button>
+      ))}
+    </ul>
+  );
+};
+
+export default Filters;
