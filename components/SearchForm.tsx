369a46bc7daec08ffb93840db0d0c90e7282d42d
filipi <filipisee@hotmail.com>
2023-09-19 18:58:41 -0300

    structuring search form

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import Image from "next/image";
+
+import { Input } from "@/components/ui/input";
 
 const SearchForm = () => {
   return (
-    <div>SearchForm</div>
-  )
-}
+    <form>
+      <label>
+        <Image
+          src="/magnifying-glass.svg"
+          alt="Search Icon"
+          className="absolute left-8"
+          width={32}
+          height={32}
+        />
+        <Input className="base-regular h-fit border-0 bg-black-400 py-6 pl-20 pr-8 text-white-800 !ring-0 !ring-offset-0 placeholder:text-white-800" />
+      </label>
+    </form>
+  );
+};
 
-export default SearchForm
+export default SearchForm;
