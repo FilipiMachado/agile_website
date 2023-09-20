1598c90b06aac67b0d7380aa831ed3270dfd4239
filipi <filipisee@hotmail.com>
2023-09-19 23:10:29 -0300

    structuring resource card component

--- sanity/actions.ts
+++ sanity/actions.ts
@@ -1,4 +1,6 @@
-import { groq } from "next-sanity";
+import { groq } from 'next-sanity';
+import { readClient } from './lib/client';
+import { buildQuery } from './utils';
 
 interface GetResourcesParams {
   query: string;
@@ -6,12 +8,52 @@ interface GetResourcesParams {
   page: string;
 }
 
-export const getResources = async (params: GetResourcesParams) => {
-    const {query, category, page} = params;
+export const getResourcesPlaylist = async () => {
+  try {
+    const resources = await readClient.fetch(
+      groq`*[_type == "resourcePlaylist"]{
+        _id,
+        title,
+        resources[0...6]->{
+          title,
+          _id,
+          downloadLink,
+          "image": poster.asset->url,
+          views,
+          category
+        }
+      }`
+    );
 
-    try {
-        
-    } catch (error) {
-        
-    }
-};
+    return resources;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export const getResources = async (params: GetResourcesParams) => {
+  const { query, category, page } = params;
+
+  try {
+    const resources = await readClient.fetch(
+      groq`${buildQuery({
+        type: 'resource',
+        query,
+        category,
+        page: parseInt(page),
+      })}{
+        title,
+        _id,
+        downloadLink,
+        "image": poster.asset->url,
+        views,
+        slug,
+        category
+      }`
+    );
+
+    return resources;
+  } catch (error) {
+    console.log(error);
+  }
+}
